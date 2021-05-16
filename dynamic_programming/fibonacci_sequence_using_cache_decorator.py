abc41d74e1720dfc707789df31b27778cb8c07ae
Chi Leung <chiguan.leung@gmail.com>
2021-05-16 14:56:52 +0100

    Calculate Fibonacci numbers using cache decorator

---
+++ dynamic_programming/fibonacci_sequence_using_cache_decorator.py
@@ -0,0 +1,14 @@
+from functools import cache
+
+
+@cache
+def f(n: int) -> int:
+    """Returns the nth Fibonacci number"""
+
+    if n == 0 or n == 1:
+        return n
+
+    return f(n - 1) + f(n - 2)
+
+
+print(f(200))
